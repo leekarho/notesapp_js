462608f34f56466c75017e1ced9797568b05928f
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-09-21 16:42:43 +0100

    notes displayed are links changing url hash

--- src/notebook-view.js
+++ src/notebook-view.js
@@ -5,8 +5,14 @@
 
   NoteBookView.prototype.returnHtml = function(){
     var noteBookArray = this.noteBook.returnAllNotes();
-    var noteArray = noteBookArray.map(note => note._text.substring(0,20));
-    return "<ul><li><div>" + noteArray.join("</div></li><li><div>") + "</div></li></ul>"
+    // var noteArray = noteBookArray.map(note => note._text.substring(0,20));
+    string = "<ul>"
+    noteBookArray.forEach(function(note) {
+      string += (`<li><div><a href="#notes/${note._id}">${note._text.substring(0,20)}</a></div></li>`)
+    })
+    string += ("</ul>")
+    return string;
+    // return "<ul><li><div>" + noteArray.join("</div></li><li><div>") + "</div></li></ul>"
   };
 
   exports.NoteBookView = NoteBookView;
