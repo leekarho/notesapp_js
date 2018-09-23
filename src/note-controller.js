40143e7632a7c5ee7183f377785565bb9db518e3
leekarho <k-h_lee@hotmail.co.uk>
2018-09-23 22:22:50 +0100

    added logic to show single note page

--- src/note-controller.js
+++ src/note-controller.js
@@ -12,5 +12,21 @@
     element.innerHTML = this.view.returnHtml();
   };
 
+  NoteController.prototype.showNote = function(note) {
+    document.getElementById("app").innerHTML = note;
+  };
+
+  NoteController.prototype.getNoteIDFromUrl = function(location){
+    return location.hash.split('#notes/')[1]
+  };
+
+  NoteController.prototype.showNoteForCurrentPage = function(){
+    this.showNote(this.getNoteIDFromUrl(window.location));
+  };
+
+  NoteController.prototype.singleNotePage = function() {
+    window.addEventListener("hashchange", this.showNoteForCurrentPage());
+  };
+
   exports.NoteController = NoteController;
 })(this);
--- src/notebook-model.js
+++ src/notebook-model.js
@@ -1,11 +1,11 @@
 (function(exports){
   function NoteBook(){
     this.allNotes = [];
-    // this._note = Note
+    this.note = Note
   }
 
   NoteBook.prototype.createNote = function(text){
-    this.allNotes.push(new Note(text));
+    this.allNotes.push(new this.note(text));
   }
 
   NoteBook.prototype.returnAllNotes = function() {
